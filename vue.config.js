32f6bf6c9370b5301c860f46b19cb3a74bc838c9
liangshan <loveliangshan@126.com>
2020-03-11 19:43:03 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,21 @@ const copyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   configureWebpack: {
     plugins: [
-      new htmlWebpackPlugin({
-        minify: {
-          removeAttributeQuotes: true,
-          removeComments: true, // 移除HTML中的注释
-          collapseWhitespace: true, // 删除空白符与换行符
-          minifyCSS: true,// 压缩内联css
-          minifyJS: true,
-          collapseBooleanAttributes: true,
-          removeCommentsFromCDATA: true, // 从脚本和样式删除的注释 默认false
-          removeScriptTypeAttributes: true, // 删除script的类型属性，在h5下面script的type默认值：text/javascript 默认值false
-          removeStyleLinkTypeAttributes: true, // 删除style的类型属性， type="text/css" 同上
+      // new htmlWebpackPlugin({
+      //   minify: {
+      //     removeAttributeQuotes: true,
+      //     removeComments: true, // 移除HTML中的注释
+      //     collapseWhitespace: true, // 删除空白符与换行符
+      //     minifyCSS: true,// 压缩内联css
+      //     minifyJS: true,
+      //     collapseBooleanAttributes: true,
+      //     removeCommentsFromCDATA: true, // 从脚本和样式删除的注释 默认false
+      //     removeScriptTypeAttributes: true, // 删除script的类型属性，在h5下面script的type默认值：text/javascript 默认值false
+      //     removeStyleLinkTypeAttributes: true, // 删除style的类型属性， type="text/css" 同上
 
-        },
-        hash: true
-      }),
+      //   },
+      //   hash: true
+      // }),
       new copyWebpackPlugin([
         {
           from: __dirname + '/src/static/',
